31df03f040756aaee35a744136386fb94c28cdad
davzarov <davidzarate2590@gmail.com>
2019-03-20 10:45:51 -0300

    configuracion de zona horaria y lenguaje 20-03-2019

--- ingtaller/settings.py
+++ ingtaller/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Asuncion'
 
 USE_I18N = True
 
