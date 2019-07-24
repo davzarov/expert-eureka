0640d60b99091416cf1ab135dead148d72a876d8
davzarov <davidzarate2590@gmail.com>
2019-07-24 11:58:18 -0400

    navegacion y urls

--- ventas/urls.py
+++ ventas/urls.py
@@ -1,13 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 from django.views.generic import TemplateView
-from . import views
 
 app_name = 'ventas'
 
 urlpatterns = [
-    path(
-        'pos',
-        login_required(TemplateView.as_view(template_name='ventas/pos.html')),
-        name='pos')
+    path('', login_required(TemplateView.as_view(template_name='ventas/pos.html')), name='pos')
 ]
